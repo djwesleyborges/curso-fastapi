3e5a4e361d666e100f069d3c9bdba537e76688f6
Wesley Borges <djwesleyborges@gmail.com>
2024-04-29 10:30:33 -0300

    feat: Implementado campo de enum para validação dos dados

--- contas_a_pagar_e_receber/routers/contas_a_pagar_e_receber_router.py
+++ contas_a_pagar_e_receber/routers/contas_a_pagar_e_receber_router.py
@@ -1,7 +1,8 @@
 from decimal import Decimal
+from enum import Enum
 
 from fastapi import APIRouter, Depends
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 
 from contas_a_pagar_e_receber.models import ContaPagarReceber
@@ -13,24 +14,28 @@ router = APIRouter(prefix="/contas_a_pagar_e_receber")
 class ContasPagarReceberResponse(BaseModel):
     id: int
     descricao: str
-    valor: Decimal
+    valor: float
     tipo: str
 
     class Config:
         orm_mode = True
 
 
+class ContaPagarReceberTipoEnum(str, Enum):
+    PAGAR = "PAGAR"
+    RECEBER = "RECEBER"
+
+
 class ContasPagarReceberRequest(BaseModel):
-    descricao: str
-    valor: Decimal
-    tipo: str
+    descricao: str = Field(min_length=5, max_length=30)
+    valor: float = Field(gt=0)
+    tipo: ContaPagarReceberTipoEnum
 
 
 @router.get("/", response_model=list[ContasPagarReceberResponse])
 def lista_contas(db: Session = Depends(get_db)) -> list[ContasPagarReceberResponse]:
     contas = db.query(ContaPagarReceber).all()
     return contas
-
 
 
 @router.post("/", response_model=ContasPagarReceberResponse, status_code=201)
